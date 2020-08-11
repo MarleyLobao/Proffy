b46ee309d3e63c81021da04b5f8b73c181c82344
Marley Lobao <marley.sousa@ee.ufcg.edu.br>
2020-08-10 23:16:28 -0300

    filtro de cadastro finalizado

--- src/database/fake_data.js
+++ src/database/fake_data.js
@@ -10,13 +10,13 @@ const proffys = [
         time_from:[720],
         time_to: [1220]
     },
-    {   name: "Marley Lobão de Sousa",
-        avatar: "https://avatars1.githubusercontent.com/u/64692533?s=460&u=fa64aefc3beddbef7400a8a2d06db928dc1c67c9&v=4",
+    {   name: "Daniela Evangelista",
+        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
         wpp: "123456789",
-        bio: "Bora bora.",
-        subject: "Matemática",
-        cost: "100",
-        weekday: [1],
+        bio: "Entusiasta das melhores tecnologias de química avançada.<br/><br/>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+        subject: "Química",
+        cost: "40",
+        weekday: [0],
         time_from:[720],
         time_to: [1220]
     }
